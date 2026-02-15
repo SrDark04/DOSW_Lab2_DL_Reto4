dd91f71d94c59475ae2023530de59b56089edc38
Roger Duran <rogermao5.1@gmail.com>
2026-02-15 12:40:44 -0500

    La creacion de la gran estafa

---
+++ LaEstafaDeLaCasaDeCambio/src/main/java/com/ejemplo/Reto4/App.java
@@ -0,0 +1,13 @@
+package com.ejemplo.Reto4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
